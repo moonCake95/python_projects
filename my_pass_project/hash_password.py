0584b08ab337831e07c4d9ab516f292e839fb5f4
moonCake95 <65113488+moonCake95@users.noreply.github.com>
2025-03-18 14:45:30 +0200

    Delete my_pass_project/hash_password.py

--- my_pass_project/hash_password.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-password = input('Enter your master password: ')
-hashed_password = hashlib.sha256(password.encode()).hexdigest()
-
-with open('hashed_password.txt', 'w') as f:
-  f.write(hashed_password)
-
-print('Hashed password saved securely. Delete this script after running it.')
